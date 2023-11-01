a2eb31aad5df5f474d65496c4d92e569ae07a120
ChrisRamirez02 <148750708+ChrisRamirez02@users.noreply.github.com>
2023-11-02 00:52:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -35,5 +35,23 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
     
+// Add event listeners to open social media links in new tabs
+document.getElementById('facebookIcon').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('https://www.facebook.com', '_blank');
+});
 
-    
+document.getElementById('twitterIcon').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('https://twitter.com', '_blank');
+});
+
+document.getElementById('instagramIcon').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('https://www.instagram.com', '_blank');
+});
+
+document.getElementById('telegramIcon').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('https://telegram.org', '_blank');
+});
